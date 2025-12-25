488def48d94a4c7c19e10c3571fbce83570d417b
darkking019 <12jonathanribe@gmail.com>
2025-12-25 18:13:31 -0300

    add

--- aprenderlaravel/app/Http/Controllers/Api/LogoutController.php
+++ aprenderlaravel/app/Http/Controllers/Api/LogoutController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class LogoutController extends Controller
 {
-    //
+    public function destroy(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out'
+        ]);
+    }
 }
+
